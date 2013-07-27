7c2d9198c397ede908b1fcba68d77c08be6cd9f8
gustavoloss <gustavolloss@gmail.com>
2013-07-27 00:42:14 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -15,7 +15,7 @@ app.get('/', function(request, response) {
 	response.send(myBuffer.toString("utf-8"));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
